4d5985ba0bac59ef47fc7af994396d983fd16429
nieyage <55966709+nieyage@users.noreply.github.com>
2023-05-15 21:59:41 +0800

    update

--- AR3_scATAC/02_scATAC_preprocessing_QC.R
+++ AR3_scATAC/02_scATAC_preprocessing_QC.R
@@ -1,10 +1,11 @@
-
-# 1. load in signac 
+#####################
+# 1. load in signac #
+#####################
 library(Signac)
 library(Seurat)
 library(GenomicRanges)
 library(future)
-
+library(ggplot2)
 # combine peaksets 
 # read in peak sets
 peaks.AR3_C4_scATAC <- read.table(
@@ -75,14 +76,180 @@ AR3_C4 <- CreateSeuratObject(AR3_C4_assay, assay = "ATAC", meta.data=md.AR3_C4_s
 AR3_C5_assay <- CreateChromatinAssay(AR3_C5_scATAC.counts, fragments = frags.AR3_C5_scATAC)
 AR3_C5 <- CreateSeuratObject(AR3_C5_assay, assay = "ATAC", meta.data=md.AR3_C5_scATAC)
 
+###############################
+# 2. calculate the qc feature #
+###############################
+# extract gene annotations from EnsDb
+annotations <- GetGRangesFromEnsDb(ensdb = EnsDb.Mmusculus.v79)
+# change to UCSC style since the data was mapped to hg19
+seqlevelsStyle(annotations) <- 'UCSC'
+objList <- c(AR3_C4,AR3_C5)
 
-# 2. calculate the qc feature 
+# calculate the score of NS and TSS
+for (i in seq_len(length(objList))) {
+    Annotation(objList[[i]]) <- annotations # add the gene information to the object
+    objList[[i]] <- NucleosomeSignal(objList[[i]])
+    objList[[i]] <- TSSEnrichment(objList[[i]],fast=FALSE)
+    }
 
-# 3. doubletfinder(ArchR or DoubletFinder?)
+# plot TSS and fragrament distribution plot 
+  pdf("./01_qc/TSS_distribution_C4.pdf")
+  objList[[1]]$high.tss<-ifelse(objList[[1]]$TSS.enrichment > 2, 'High', 'Low')
+  TSS<-TSSPlot(objList[[1]], group.by = 'high.tss') + NoLegend()+ labs(title = "NE")
+  objList[[1]]$NS <- ifelse(objList[[1]]$nucleosome_signal > 4, 'NS > 4', 'NS < 4')
+  Frag<-FragmentHistogram(object = objList[[1]], group.by = 'NS',region = "chr1-1-195471971")+ labs(title = "NE")
+  print(TSS);
+  print(Frag);
+  dev.off();
+  pdf("./01_qc/TSS_distribution_C5.pdf")
+  objList[[2]]$high.tss<-ifelse(objList[[2]]$TSS.enrichment > 2, 'High', 'Low')
+  TSS<-TSSPlot(objList[[2]], group.by = 'high.tss') + NoLegend()+ labs(title = "Nurse")
+  objList[[2]]$NS <- ifelse(objList[[2]]$nucleosome_signal > 4, 'NS > 4', 'NS < 4')
+  Frag<-FragmentHistogram(object = objList[[2]], group.by = 'NS',region = "chr1-1-195471971")+ labs(title = "Nurse")
+  print(TSS);
+  print(Frag);
+  dev.off();
 
-# 4. filter cells (low quality and doublets)
+objList[[1]]$pct_reads_in_peaks <- objList[[1]]$peak_region_fragments / objList[[1]]$passed_filters * 100
+objList[[1]]$blacklist_ratio <- objList[[1]]$blacklist_region_fragments / objList[[1]]$peak_region_fragments
+pdf("./01_qc/QC_NS_TSS_density_C4.pdf",width=8,height=4)
+plot(density(objList[[1]]@meta.data$nucleosome_signal),xlim=c(0,4))
+plot(density(objList[[1]]@meta.data$TSS.enrichment),xlim=c(0,20))
+plot(density(objList[[1]]@meta.data$nCount_ATAC),xlim=c(0,200000))
+plot(density(objList[[1]]@meta.data$nFeature_ATAC),xlim=c(0,50000))
+plot(density(objList[[1]]@meta.data$pct_reads_in_peaks),xlim=c(0,100))
+plot(density(objList[[1]]@meta.data$blacklist_ratio),xlim=c(0,20000))
+dev.off()
 
-# 5. intergrated object and analyze 
+objList[[2]]$pct_reads_in_peaks <- objList[[2]]$peak_region_fragments / objList[[2]]$passed_filters * 100
+objList[[2]]$blacklist_ratio <- objList[[2]]$blacklist_region_fragments / objList[[2]]$peak_region_fragments
+pdf("./01_qc/QC_NS_TSS_density_C5.pdf",width=8,height=4)
+plot(density(objList[[2]]@meta.data$nucleosome_signal),xlim=c(0,4))
+plot(density(objList[[2]]@meta.data$TSS.enrichment),xlim=c(0,20))
+plot(density(objList[[2]]@meta.data$nCount_ATAC),xlim=c(0,200000))
+plot(density(objList[[2]]@meta.data$nFeature_ATAC),xlim=c(0,50000))
+plot(density(objList[[2]]@meta.data$pct_reads_in_peaks),xlim=c(0,100))
+plot(density(objList[[2]]@meta.data$blacklist_ratio),xlim=c(0,20000))
+dev.off()
 
-# 6. annotate all cell type in AR3 
+sample<-c("AR3_C4","AR3_C5")
+for (i in seq_len(length(objList))) {
+  # plot QC plot 
+  pdf(file = paste("./01_qc/QC_before_Vlnplot_",sample[i],".pdf", sep = ""),width=10,height=6)
+  qc<-VlnPlot(object = objList[[i]],
+            features = c('pct_reads_in_peaks', 'peak_region_fragments','TSS.enrichment', 'nucleosome_signal'),
+            ncol = 3,
+            pt.size = 0.01
+          )
+  print(qc)
+  dev.off();
+   }
+
+#############################################
+# 3. doubletfinder(ArchR) #
+#############################################
+library(ArchR)
+addArchRThreads(threads = 1) 
+addArchRGenome("mm10")
+inputFiles <- c("/md01/nieyg/project/lineage_tracing/heart_regeneration/00_data/AR3_data/scATAC/AR3_C4_scATAC/outs/fragments.tsv.gz",
+  "/md01/nieyg/project/lineage_tracing/heart_regeneration/00_data/AR3_data/scATAC/AR3_C5_scATAC/outs/fragments.tsv.gz")
+names(inputFiles)<- c("AR3_C4","AR3_C5")
+ArrowFiles <- createArrowFiles(
+  inputFiles = inputFiles,
+  sampleNames = names(inputFiles),
+  filterTSS = 4, #Dont set this too high because you can always increase later
+  filterFrags = 1000, 
+  addTileMat = TRUE,
+  addGeneScoreMat = TRUE
+)
+doubScores <- addDoubletScores(
+    input = ArrowFiles,
+    k = 10, #Refers to how many cells near a "pseudo-doublet" to count.
+    knnMethod = "UMAP", #Refers to the embedding to use for nearest neighbor search with doublet projection.
+    LSIMethod = 1
+)
+projHeme1 <- ArchRProject(
+  ArrowFiles = ArrowFiles, 
+  outputDirectory = "02_OSN_ArchR",
+  copyArrows = TRUE #This is recommened so that if you modify the Arrow files you have an original copy for later usage.
+)
+df <- getCellColData(projHeme1, select = c("log10(nFrags)", "TSSEnrichment"))
+p <- ggPoint(
+    x = df[,1], 
+    y = df[,2], 
+    colorDensity = TRUE,
+    continuousSet = "sambaNight",
+    xlabel = "Log10 Unique Fragments",
+    ylabel = "TSS Enrichment",
+    xlim = c(log10(500), quantile(df[,1], probs = 0.99)),
+    ylim = c(0, quantile(df[,2], probs = 0.99))
+) + geom_hline(yintercept = 4, lty = "dashed") + geom_vline(xintercept = 3, lty = "dashed")
+plotPDF(p, name = "TSS-vs-Frags.pdf", ArchRProj = projHeme1, addDOC = FALSE)
+
+p1 <- plotFragmentSizes(ArchRProj = projHeme1)
+p2 <- plotTSSEnrichment(ArchRProj = projHeme1)
+plotPDF(p1,p2, name = "QC-Sample-FragSizes-TSSProfile.pdf", ArchRProj = projHeme1, addDOC = FALSE, width = 5, height = 5)
+projHeme2 <- filterDoublets(projHeme1)
+
+filterDoublets_C4<- projHeme2$cellNames[grep("AR3_C4",projHeme2$cellNames)]
+filterDoublets_C4<- gsub("AR3_C4#","",filterDoublets_C4)
+filterDoublets_C5<- projHeme2$cellNames[grep("AR3_C5",projHeme2$cellNames)]
+filterDoublets_C5<- gsub("AR3_C5#","",filterDoublets_C5)
+
+
+
+##############################################
+# 4. filter cells (low quality and doublets) #
+##############################################
+
+# filter out low quality cells
+# To remove doublets,select different cutoff#####
+# NE 
+  objList2<-c()
+  objList2[[1]]<-subset(x=objList[[1]],
+   subset = nCount_ATAC < 100000 &nCount_ATAC > 1000 &
+    nucleosome_signal < 4 &
+    TSS.enrichment > 2 &
+    peak_region_fragments > 1000 &
+    peak_region_fragments < 100000 &
+    pct_reads_in_peaks > 25 
+    )
+  print(objList2[[1]])
+# Nurse 
+  objList2[[2]]<-subset(x=objList[[2]],
+   subset = nCount_ATAC < 100000 &nCount_ATAC > 1000 &
+    nucleosome_signal < 4 &
+    TSS.enrichment > 2 &
+    peak_region_fragments > 1000 &
+    peak_region_fragments < 100000 &
+    pct_reads_in_peaks > 25 
+    )
+  print(objList2[[2]])
+
+# Subset identified singlets
+last_C4<-intersect(colnames(objList2[[1]]),filterDoublets_C4)
+AR3_C4_last<- subset(objList2[[1]],cells=last_C4)
+last_C5<-intersect(colnames(objList2[[2]]),filterDoublets_C5)
+AR3_C5_last<- subset(objList2[[2]],cells=last_C5)
+saveRDS(AR3_C4_last,"./03_all_celltype/AR3_C4_scATAC.rds")
+saveRDS(AR3_C5_last,"./03_all_celltype/AR3_C5_scATAC.rds")
+
+#####################################
+# 5. intergrated object and analyze #
+#####################################
+combined <- merge(
+  x = AR3_C4_last,
+  y = AR3_C5_last,
+  add.cell.ids = c("AR3_C4", "AR3_C5")
+)
+combined[["ATAC"]]
+combined <- RunTFIDF(combined)
+combined <- FindTopFeatures(combined, min.cutoff = 20)
+combined <- RunSVD(combined)
+combined <- RunUMAP(combined, dims = 2:50, reduction = 'lsi')
+saveRDS(combined,"./03_all_celltype/combined_AR3_scATAC.rds")
+
+####################################
+# 6. annotate all cell type in AR3 #
+####################################
 
--- plogF1met5d/01_Signac_filter_cell.R
+++ plogF1met5d/01_Signac_filter_cell.R
@@ -52,7 +52,7 @@ plot(density(OSN@meta.data$TSS.enrichment),xlim=c(0,20))
 plot(density(OSN@meta.data$nCount_ATAC),xlim=c(0,200000))
 plot(density(OSN@meta.data$nFeature_ATAC),xlim=c(0,50000))
 plot(density(OSN@meta.data$pct_reads_in_peaks),xlim=c(0,100))
-plot(density(OSN@meta.data$peak_region_fragments),xlim=c(0,100000))
+plot(density(OSN@meta.data$atac_peak_region_fragments),xlim=c(0,20000))
 dev.off()
 
 #OSN$blacklist_ratio <- OSN$blacklist_region_fragments / OSN$atac_peak_region_fragments
@@ -68,8 +68,8 @@ dev.off()
 # filter cells 
 OSN <- subset(
   x = OSN,
-  subset = peak_region_fragments > 3000 &
-    peak_region_fragments < 100000 &
+  subset = atac_peak_region_fragments > 1000 &
+    atac_peak_region_fragments < 100000 &
     pct_reads_in_peaks > 40 &
     nucleosome_signal < 4 &
     TSS.enrichment > 2 &
@@ -77,60 +77,9 @@ OSN <- subset(
     nCount_ATAC < 100000  
 )
 
-# doubletFinder 
-  dataset.name <-"OSN"
-  sratDecontx <- OSN
-  # Compute expected doublet rate
-  cellN=nrow(sratDecontx@meta.data)
-  expDoubletRate = (cellN*0.0008 + 0.0527)*0.01
-  normalizationMethod='SCTransform'
-  sweep.res.list_scData <- paramSweep_v3(sratDecontx, 
-                                         PCs = 1:50, 
-                                         sct = normalizationMethod == 'SCTransform', 
-                                         num.cores = 4) #num.cores = 4
-  sweep.stats_scData <- summarizeSweep(sweep.res.list_scData, GT = FALSE)
-  bcmvn_scData <- find.pK(sweep.stats_scData)
-  bcmvn_scData$pK <- as.numeric(as.character(bcmvn_scData$pK))
-  pK1=bcmvn_scData$pK[bcmvn_scData$BCmetric==max(bcmvn_scData$BCmetric)]
-  print(head(pK1))
-  # PLOT: pK selection
-  p1=ggplot(data=bcmvn_scData, 
-            aes(x=pK, y=BCmetric, group=2)) +
-    geom_line(color="blue")+
-    geom_point()+
-    geom_vline(xintercept=pK1, linetype="dashed", color = "red")+
-    labs(title="pK Selection",x="pK", y = "BCmvn")+
-    theme_classic()
-  filename <- paste0("./01_doubletfinder/2a_doubletfinder_pkselection_","OSN",".pdf")
-  ggsave(filename, limitsize = FALSE, units = "px", width = 8000, height = 4000,p1)
-  # More doublet finder
-  pK1=as.numeric(as.character( pK1 ))
-  ## Homotypic Doublet Proportion Estimate -------------------------------------------------------------------------------------
-  annotations <- sratDecontx@meta.data$seurat_clusters
-  homotypic.prop <- modelHomotypic(annotations)   
-  nExp_poi <- round(expDoubletRate*nrow(sratDecontx@meta.data))  ## Assuming 7.5% doublet formation rate - tailor for your dataset
-  nExp_poi.adj <- round(nExp_poi*(1-homotypic.prop))
-  sratDecontx <- doubletFinder_v3( sratDecontx, PCs = sratDecontx@commands$RunUMAP.SCT.pca$dims,
-                                   pN = 0.25, pK = pK1, nExp = nExp_poi, reuse.pANN = FALSE, sct = TRUE)
-  sratDecontx@meta.data$DoubletFinder =  sratDecontx@meta.data[,grep('DF.classifications', colnames( sratDecontx@meta.data))]
-  # PLOT: Doublet Finder graphs
-  p2 <- FeatureScatter(sratDecontx, feature1 = "nCount_ATAC", feature2 = "percent.mt", group.by = 'DoubletFinder')
-  p3 <- FeatureScatter(sratDecontx, feature1 = "nCount_ATAC", feature2 = "nFeature_RNA", group.by = 'DoubletFinder')
-  g = arrangeGrob(p2,p3, ncol = 2)
-  filename <- paste0("./01_doubletfinder/2b_doubletfinder_ScatterPlots_",dataset.name,".pdf")
-  ggsave(filename, limitsize = FALSE, units = "px", width = 8000, height = 4000,g)
-  rm(filename, g)
-  # PLOT: Violin Plots
-  p4 <- VlnPlot(sratDecontx, features = c('nFeature_RNA', 'nCount_RNA', 'percent.mt'), group.by = 'DoubletFinder', pt.size = 0)
-  filename <- paste0("./01_doubletfinder/2c_doubletfinder_DoubletFinder-ViolinPlots_",dataset.name,".pdf")
-  ggsave(filename, limitsize = FALSE, units = "px", width = 8000, height = 4000,p4)
-  objList2[[i]] <- sratDecontx
-
-# create the geneactivate matrix 
-
 # ArchR 
 library(ArchR)
-addArchRThreads(threads = 16) 
+addArchRThreads(threads = 1) 
 addArchRGenome("mm10")
 inputFiles <- "/md01/nieyg/project/lineage_tracing/OSN_regeneration/00_data/plogF1met5d_masked/outs/atac_fragments.tsv.gz"
 names(inputFiles)<-"OSN"
@@ -143,13 +92,21 @@ ArrowFiles <- createArrowFiles(
   addTileMat = TRUE,
   addGeneScoreMat = TRUE
 )
+doubScores <- addDoubletScores(
+    input = ArrowFiles,
+    k = 10, #Refers to how many cells near a "pseudo-doublet" to count.
+    knnMethod = "UMAP", #Refers to the embedding to use for nearest neighbor search with doublet projection.
+    LSIMethod = 1
+)
+
 projHeme1 <- ArchRProject(
   ArrowFiles = ArrowFiles, 
-  outputDirectory = "03_OSN_ArchR",
+  outputDirectory = "02_OSN_ArchR",
   copyArrows = TRUE #This is recommened so that if you modify the Arrow files you have an original copy for later usage.
 )
 getAvailableMatrices(projHeme1)
 head(projHeme1$cellNames)
+df <- getCellColData(projHeme1, select = c("log10(nFrags)", "TSSEnrichment"))
 p <- ggPoint(
     x = df[,1], 
     y = df[,2], 
@@ -161,35 +118,24 @@ p <- ggPoint(
     ylim = c(0, quantile(df[,2], probs = 0.99))
 ) + geom_hline(yintercept = 4, lty = "dashed") + geom_vline(xintercept = 3, lty = "dashed")
 
-p
 plotPDF(p, name = "TSS-vs-Frags.pdf", ArchRProj = projHeme1, addDOC = FALSE)
 
 p1 <- plotFragmentSizes(ArchRProj = projHeme1)
-p1
 p2 <- plotTSSEnrichment(ArchRProj = projHeme1)
-p2
-plotPDF(p1,p2, name = "QC-Sample-FragSizes-TSSProfile.pdf", ArchRProj = projHeme1, addDOC = FALSE, width = 5, height = 5)
-saveArchRProject(ArchRProj = projHeme1, outputDirectory = "Save-ProjHeme1", load = FALSE)
+plotPDF(p1,p2, name = "./02_OSN_ArchR/QC-Sample-FragSizes-TSSProfile.pdf", ArchRProj = projHeme1, addDOC = FALSE, width = 5, height = 5)
+saveArchRProject(ArchRProj = projHeme1, outputDirectory = "Save-ProjHeme1-for-filterdoublets", load = FALSE)
+
 projHeme2 <- filterDoublets(projHeme1)
+filterDoublets<- gsub("OSN#","",projHeme2$cellNames)
+last<-intersect(colnames(OSN),filterDoublets)
+OSN_last<- subset(OSN,cells=last)
 
-
-
-
-doubScores <- addDoubletScores(
-    input = ArrowFiles,
-    k = 10, #Refers to how many cells near a "pseudo-doublet" to count.
-    knnMethod = "UMAP", #Refers to the embedding to use for nearest neighbor search with doublet projection.
-    LSIMethod = 1
-)
-
-
-
-OSN
-OSN <- RunTFIDF(OSN)
+OSN <- RunTFIDF(OSN_last)
 OSN <- FindTopFeatures(OSN, min.cutoff = 'q0')
 OSN <- RunSVD(object = OSN)
-
+pdf("./03_All_celltype/DepthCor.pdf")
 DepthCor(OSN)
+dev.off()
 OSN <- RunUMAP(
   object = OSN,
   reduction = 'lsi',
@@ -206,53 +152,70 @@ OSN <- FindClusters(
   resolution = 1.2,
   verbose = FALSE
 )
-
+table(OSN$seurat.clusters)
+pdf("./03_All_celltype/OSN_ATAC_Umap.pdf",width=6,height=6)
 DimPlot(object = OSN, label = TRUE) + NoLegend()
+dev.off()
+saveRDS(OSN, file = "./03_All_celltype/OSN_ATAC_all_celltype.rds")
+
 # compute gene activities
 gene.activities <- GeneActivity(OSN)
-
 # add the gene activity matrix to the Seurat object as a new assay
 OSN[['RNA']] <- CreateAssayObject(counts = gene.activities)
+
 OSN <- NormalizeData(
   object = OSN,
   assay = 'RNA',
   normalization.method = 'LogNormalize',
   scale.factor = median(OSN$nCount_RNA)
 )
+
+## Load the pre-processed scRNA-seq data
+#allen_rna <- readRDS("../vignette_data/allen_OSN.rds")
+#allen_rna <- FindVariableFeatures(
+#  object = allen_rna,
+#  nfeatures = 5000
+#)
+#
+#transfer.anchors <- FindTransferAnchors(
+#  reference = allen_rna,
+#  query = OSN,
+#  reduction = 'cca',
+#  dims = 1:40
+#)
+#
+#predicted.labels <- TransferData(
+#  anchorset = transfer.anchors,
+#  refdata = allen_rna$subclass,
+#  weight.reduction = OSN[['lsi']],
+#  dims = 2:30
+#)
+#
+#OSN <- AddMetaData(object = OSN, metadata = predicted.labels)
+#plot1 <- DimPlot(allen_rna, group.by = 'subclass', label = TRUE, repel = TRUE) + NoLegend() + ggtitle('scRNA-seq')
+#plot2 <- DimPlot(OSN, group.by = 'predicted.id', label = TRUE, repel = TRUE) + NoLegend() + ggtitle('scATAC-seq')
+#plot1 + plot2
+
+###annottaion 
 DefaultAssay(OSN) <- 'RNA'
-FeaturePlot(
-  object = OSN,
-  features = c('Sst','Pvalb',"Gad2","Neurod6","Rorb","Syt6"),
-  pt.size = 0.1,
-  max.cutoff = 'q95',
-  ncol = 3
+pdf("./03_All_celltype/cluster_dotplot_OSN_all.pdf")
+features <- c(
+              "Omp","Gng13",#成熟OSN
+              "Nqo1","Ncam2",
+              "Gap43","Gng8",#不成熟OSN
+              "Sox2","Ermn","Cyp2g1",#支持细胞
+              "Neurog1","Neurod1",#INP
+              "Ascl1","Kit" ,#GBC
+              "Mki67","Top2a",#cycling
+              "Trp63","Krt5","Krt14",#HBC
+              "Cxcl14","Meg3", #olfactory HBCs were distinguished from respiratory HBCs
+              "Reg3g", #respiratory
+              "Ascl3","Cftr",#olfactory microvillar cells
+              "Krt18","Trpm5" #brush cell
+            
 )
-# Load the pre-processed scRNA-seq data
-allen_rna <- readRDS("../vignette_data/allen_OSN.rds")
-allen_rna <- FindVariableFeatures(
-  object = allen_rna,
-  nfeatures = 5000
-)
-
-transfer.anchors <- FindTransferAnchors(
-  reference = allen_rna,
-  query = OSN,
-  reduction = 'cca',
-  dims = 1:40
-)
-
-predicted.labels <- TransferData(
-  anchorset = transfer.anchors,
-  refdata = allen_rna$subclass,
-  weight.reduction = OSN[['lsi']],
-  dims = 2:30
-)
-
-OSN <- AddMetaData(object = OSN, metadata = predicted.labels)
-plot1 <- DimPlot(allen_rna, group.by = 'subclass', label = TRUE, repel = TRUE) + NoLegend() + ggtitle('scRNA-seq')
-plot2 <- DimPlot(OSN, group.by = 'predicted.id', label = TRUE, repel = TRUE) + NoLegend() + ggtitle('scATAC-seq')
-plot1 + plot2
-
-
+DotPlot(OSN, features = features,dot.scale = 3) + RotatedAxis() + theme(axis.text.x=element_text(size=8))
+dev.off()
+saveRDS(OSN, file = "./03_All_celltype/OSN_ATAC_GeneActivity_all_celltype.rds")
 
 
